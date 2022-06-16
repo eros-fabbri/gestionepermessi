5f68007c7173e1dc8794279e5407bc85ac6000df
eros fabbri <erosfabbridev@gmail.com>
2022-06-16 13:22:33 +0200

    implementazione base dei services necessari  al login

---
+++ src/main/java/it/prova/gestionepermessi/repository/ruolo/RuoloRepository.java
@@ -0,0 +1,10 @@
+package it.prova.gestionepermessi.repository.ruolo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.gestionepermessi.model.Ruolo;
+
+
+public interface RuoloRepository extends CrudRepository<Ruolo, Long> {
+	Ruolo findByDescrizioneAndCodice(String descrizione, String codice);
+}
